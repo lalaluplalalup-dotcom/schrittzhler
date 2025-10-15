e928aa62ee1e3a05e7bf01125e298a345bc95bcd
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2025-10-15 10:56:15 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,20 @@
 input.onGesture(Gesture.Shake, function () {
-    schrittzähler += 1
-    basic.showNumber(schrittzähler)
-    if (schrittzähler == 5) {
-        music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.UntilDone)
-        radio.sendNumber(schrittzähler)
+    if (is_ready == 1) {
+        schrittzähler += 1
+        basic.showNumber(schrittzähler)
+        if (schrittzähler == 5) {
+            music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.UntilDone)
+        }
     }
 })
 let schrittzähler = 0
+let is_ready = 0
+let countdown = 5
+is_ready = 0
+for (let Index = 0; Index <= countdown; Index++) {
+    basic.showNumber(countdown - Index)
+    basic.pause(100)
+}
+music.playTone(262, music.beat(BeatFraction.Whole))
+is_ready = 1
 schrittzähler = 0
-radio.setGroup(1)
-basic.showNumber(schrittzähler)
-basic.forever(function () {
-	
-})
